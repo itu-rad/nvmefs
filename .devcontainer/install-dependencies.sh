e17f2749ada899852d44b9d99fbd59f4f342e5bf
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-02-05 10:43:59 +0100

    Add vcpkg as a installed dependency to dev container

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -4,4 +4,18 @@
 bash -i ./scripts/xnvme/install.sh
 
 # Install dev dependencies
-apk --no-cache add cmake ccache
+apk update
+apk add --upgrade libcurl
+apk --no-cache add cmake ccache gnupg vim curl zip
+
+# Setup tools directory
+mkdir -p /root/.tools/bin
+cd /root/.tools
+
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg && ./bootstrap-vcpkg.sh
+
+VCPKG_ROOT=~/.tools/vcpkg
+
+echo "export VCPKG_ROOT='$VCPKG_ROOT'" >> ~/.bashrc
+echo "export PATH='$PATH:$VCPKG_ROOT'" >> ~/.bashrc 

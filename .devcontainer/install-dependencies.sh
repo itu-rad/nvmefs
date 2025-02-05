e3f099c1639e573e983ed782ef470d2dada30856
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-02-05 16:03:51 +0100

    Create new dev container with missing dependencies (#4)
    * Add editorconfig extension to dev container
    
    * Change default shell from ash --> bash
    
    * Add vcpkg as a installed dependency to dev container
    
    * Preinstall gpg to make the dev container extension configure the gpg socket connection at startup
    
    * Add new dev container compatible with both arm and x86 cpu

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # Install xnvme libraries 
-bash -i ./scripts/xnvme/install.sh
+sudo bash -i ./scripts/xnvme/install.sh
 
 # Install dev dependencies
-apk --no-cache add cmake ccache
+sudo apt update
+sudo apt install -y ccache vim curl zip
+
+# Setup tools directory
+mkdir -p ~/.tools/bin
+cd ~/.tools
+
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg && ./bootstrap-vcpkg.sh
+
+VCPKG_ROOT=~/.tools/vcpkg
+
+echo "export VCPKG_ROOT='$VCPKG_ROOT'" >> ~/.bashrc
+echo "export PATH='$PATH:$VCPKG_ROOT'" >> ~/.bashrc 

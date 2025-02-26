8da5636598d833374562a4248d3999b2b9e376a7
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-02-26 10:26:12 +0100

    Use DuckDB memory allocator to create "external" buffer 🥇  (#15)
    * init: create initial skelekton for lba manager
    
    * Add reference to clang files
    
    * Format files using the clang-format spec
    
    * Change config print function data type
    
    * Add temp_directory to config print output
    
    * Remove hello world example
    
    * Refactor debug print
    
    * Use defined nvmefs metadata location
    
    * Add duckdb allocator to proxy
    
    * Use the correct extension prefix in file paths
    
    * Remove old metadata files
    
    ---------
    
    Co-authored-by: emiloh <82398520+emiloh@users.noreply.github.com>

--- src/include/nvmefs.hpp
+++ src/include/nvmefs.hpp
@@ -2,68 +2,67 @@
 
 #include "duckdb/common/file_system.hpp"
 #include "duckdb/common/map.hpp"
-#include <libxnvme.h>
 #include "nvmefs_proxy.hpp"
 
+#include <libxnvme.h>
+
 #define NVMEFS_PATH_PREFIX "nvmefs://"
-#define FDP_PLID_COUNT 8
+#define FDP_PLID_COUNT     8
 
-namespace duckdb
-{
+namespace duckdb {
 
-	struct NvmeCmdContext
-	{
-		xnvme_cmd_ctx ctx;
-		uint32_t namespace_id;
-		uint64_t lba_size;
-	};
+struct NvmeCmdContext {
+	xnvme_cmd_ctx ctx;
+	uint32_t namespace_id;
+	uint64_t lba_size;
+};
 
-	class NvmeFileHandle : public FileHandle
-	{
+class NvmeFileHandle : public FileHandle {
 	friend class NvmeFileSystem;
-	public:
-		NvmeFileHandle(FileSystem &file_system, string path, uint8_t plid_idx, xnvme_dev *device);
-		~NvmeFileHandle() override;
 
-		void Read(void *buffer, idx_t nr_bytes, idx_t location);
-		void Write(void *buffer, idx_t nr_bytes, idx_t location);
-		int64_t Read(void *buffer, idx_t nr_bytes);
-		int64_t Write(void *buffer, idx_t nr_bytes);
+public:
+	NvmeFileHandle(FileSystem &file_system, string path, uint8_t plid_idx, xnvme_dev *device);
+	~NvmeFileHandle() override;
 
-		unique_ptr<NvmeCmdContext> PrepareWriteCommand();
-		unique_ptr<NvmeCmdContext> PrepareReadCommand();
+	void Read(void *buffer, idx_t nr_bytes, idx_t location);
+	void Write(void *buffer, idx_t nr_bytes, idx_t location);
+	int64_t Read(void *buffer, idx_t nr_bytes);
+	int64_t Write(void *buffer, idx_t nr_bytes);
 
-		void Close() {}
+	unique_ptr<NvmeCmdContext> PrepareWriteCommand();
+	unique_ptr<NvmeCmdContext> PrepareReadCommand();
 
-	protected:
-		xnvme_dev *device;
-		uint32_t placement_identifier;
-	};
+	void Close() {
+	}
 
-	class NvmeFileSystemProxy;
-	class NvmeFileSystem : public FileSystem
-	{
-	public:
-		NvmeFileSystem(NvmeFileSystemProxy &proxy_ref);
-		unique_ptr<FileHandle> OpenFile(const string &path, FileOpenFlags flags, optional_ptr<FileOpener> opener = nullptr) override;
-		void Read(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) override;
-		void Write(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) override;
-		int64_t Read(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
-		int64_t Write(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
-		bool CanHandleFile(const string &fpath) override;
+protected:
+	xnvme_dev *device;
+	uint32_t placement_identifier;
+};
 
-		string GetName() const
-		{
-			return "NvmeFileSystem";
-		}
+class NvmeFileSystemProxy;
+class NvmeFileSystem : public FileSystem {
+public:
+	NvmeFileSystem(NvmeFileSystemProxy &proxy_ref);
+	unique_ptr<FileHandle> OpenFile(const string &path, FileOpenFlags flags,
+	                                optional_ptr<FileOpener> opener = nullptr) override;
+	void Read(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) override;
+	void Write(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) override;
+	int64_t Read(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
+	int64_t Write(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
+	bool CanHandleFile(const string &fpath) override;
 
-	protected:
-		uint8_t GetPlacementIdentifierIndexOrDefault(const string &path);
+	string GetName() const {
+		return "NvmeFileSystem";
+	}
 
-	private:
-		map<string, uint8_t> allocated_placement_identifiers;
-		vector<string> allocated_paths;
-		NvmeFileSystemProxy &proxy_filesystem;
-	};
+protected:
+	uint8_t GetPlacementIdentifierIndexOrDefault(const string &path);
 
-}
+private:
+	map<string, uint8_t> allocated_placement_identifiers;
+	vector<string> allocated_paths;
+	NvmeFileSystemProxy &proxy_filesystem;
+};
+
+} // namespace duckdb
--- src/nvmefs_proxy.cpp
+++ src/nvmefs_proxy.cpp
@@ -1,101 +1,112 @@
 #include "nvmefs_proxy.hpp"
-#include <iostream>
 
+#include "duckdb/main/extension_util.hpp"
+
+#include <iostream>
 
 namespace duckdb {
 
-	const char MAGIC_BYTES[] = "NVMEFS";
+const char MAGIC_BYTES[] = "NVMEFS";
 
-	NvmeFileSystemProxy::NvmeFileSystemProxy() : fs(make_uniq<NvmeFileSystem>(*this)) {
-		metadata = LoadMetadata();
-
-		// print for debug
-		std::cout << "this is metadata db - start: " << metadata.database.start << " end: " << metadata.database.end << " loc: " << metadata.database.location << std::endl;
-		std::cout << "this is metadata wal - start: " << metadata.write_ahead_log.start << " end: " << metadata.write_ahead_log.end << " loc: " << metadata.write_ahead_log.location << std::endl;
-		std::cout << "this is metadata temp - start: " << metadata.temporary.start << " end: " << metadata.temporary.end << " loc: " << metadata.temporary.location << std::endl;
-	}
-
-	void NvmeFileSystemProxy::Read(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) {
-		fs->Read(handle, buffer, nr_bytes, location);
-	}
-
-
-	void NvmeFileSystemProxy::Write(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) {
-		fs->Write(handle, buffer, nr_bytes, location);
-	}
-
-	unique_ptr<FileHandle> NvmeFileSystemProxy::OpenFile(const string &path, FileOpenFlags flags, optional_ptr<FileOpener> opener) {
-		return fs->OpenFile(path, flags, opener);
-	}
-
-	bool NvmeFileSystemProxy::CanHandleFile(const string &fpath) {
-		return fs->CanHandleFile(fpath);
-	}
-
-	GlobalMetadata NvmeFileSystemProxy::LoadMetadata() {
-		// Open file check magic bytes
-		// if no magic bytes/not equal
-		// InitializeMetadata()
-		// else proceed ...
-		idx_t bytes_to_read = sizeof(MAGIC_BYTES) + sizeof(GlobalMetadata);
-		idx_t metadata_location = 0;
-		vector<uint8_t> buf(bytes_to_read);
-		FileOpenFlags flags = FileOpenFlags::FILE_FLAGS_READ;
-
-		unique_ptr<FileHandle> fh = fs->OpenFile(NVME_GLOBAL_METADATA_PATH, flags);
-
-		fs->Read(*fh, buf.data(), bytes_to_read, metadata_location);
-
-		// Check magic bytes
-		if (memcmp(buf.data(), MAGIC_BYTES, sizeof(MAGIC_BYTES)) != 0) {
-			return InitializeMetadata();
-		}
-
-		GlobalMetadata global;
-		memcpy(&global, (buf.data() + sizeof(MAGIC_BYTES)), sizeof(GlobalMetadata));
-
-		return global;
-	}
-
-	void NvmeFileSystemProxy::WriteMetadata() {
-
-	}
-
-	uint64_t NvmeFileSystemProxy::GetLBA(MetadataType type, std::string filename) {
-
-	}
-
-	GlobalMetadata NvmeFileSystemProxy::InitializeMetadata() {
-		// Create buffer
-		// insert magic bytes
-		// insert metadata
-		// 	- We know size of db and WAL metadata and partly temp
-		// 	- We do not know the size of the file mapping field in temp
-		// 		- Should this be constant based on the size of directory?
-		//		- Dynamic?
-		// Example:
-		//  1 GB temp data -> x files -> map that supports x files total (this is the size)
-
-		idx_t bytes_to_write = sizeof(MAGIC_BYTES) + sizeof(GlobalMetadata);
-		idx_t medata_location = 0;
-
-		vector<uint8_t> buf(bytes_to_write);
-
-		Metadata meta_db{.start=1, .end=5001, .location=1};
-		Metadata meta_wal{.start=5002, .end=10002, .location=5002};
-		Metadata meta_temp{.start=10003, .end=15003, .location=10003};
-
-		GlobalMetadata global{.database=meta_db, .write_ahead_log=meta_wal, .temporary=meta_temp};
-
-		memcpy(buf.data(), MAGIC_BYTES, sizeof(MAGIC_BYTES));
-		memcpy(buf.data()+sizeof(MAGIC_BYTES), &global, sizeof(GlobalMetadata));
-
-		FileOpenFlags flags = FileOpenFlags::FILE_FLAGS_WRITE | FileOpenFlags::FILE_FLAGS_FILE_CREATE;
-
-		unique_ptr<FileHandle> fh = fs->OpenFile(NVME_GLOBAL_METADATA_PATH, flags);
-		fs->Write(*fh, buf.data(), bytes_to_write, medata_location);
-
-		return global;
-	}
-
+#ifdef DEBUG
+void PrintMetadata(Metadata &meta, string name) {
+	printf("Metadata for %s\n", name.c_str());
+	std::cout << "start: " << meta.start << " end: " << meta.end << " loc: " << meta.location << std::endl;
 }
+#else
+void PrintMetadata(Metadata &meta, string name) {
+}
+#endif
+
+NvmeFileSystemProxy::NvmeFileSystemProxy()
+    : fs(make_uniq<NvmeFileSystem>(*this)), allocator(Allocator::DefaultAllocator()) {
+	metadata = LoadMetadata();
+
+	PrintMetadata(metadata.database, "database");
+	PrintMetadata(metadata.write_ahead_log, "write_ahead_log");
+	PrintMetadata(metadata.temporary, "temporary");
+}
+
+void NvmeFileSystemProxy::Read(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) {
+	fs->Read(handle, buffer, nr_bytes, location);
+}
+
+void NvmeFileSystemProxy::Write(FileHandle &handle, void *buffer, int64_t nr_bytes, idx_t location) {
+	fs->Write(handle, buffer, nr_bytes, location);
+}
+
+unique_ptr<FileHandle> NvmeFileSystemProxy::OpenFile(const string &path, FileOpenFlags flags,
+                                                     optional_ptr<FileOpener> opener) {
+	return fs->OpenFile(path, flags, opener);
+}
+
+bool NvmeFileSystemProxy::CanHandleFile(const string &fpath) {
+	return fs->CanHandleFile(fpath);
+}
+
+GlobalMetadata NvmeFileSystemProxy::LoadMetadata() {
+
+	idx_t bytes_to_read = sizeof(MAGIC_BYTES) + sizeof(GlobalMetadata);
+	data_ptr_t buffer = allocator.AllocateData(bytes_to_read);
+	FileOpenFlags flags = FileOpenFlags::FILE_FLAGS_READ;
+
+	unique_ptr<FileHandle> fh = fs->OpenFile(NVME_GLOBAL_METADATA_PATH, flags);
+
+	fs->Read(*fh, buffer, bytes_to_read, NVMEFS_METADATA_LOCATION);
+
+	// Check magic bytes
+	if (memcmp(buffer, MAGIC_BYTES, sizeof(MAGIC_BYTES)) != 0) {
+		allocator.FreeData(buffer, bytes_to_read);
+		return InitializeMetadata();
+	}
+
+	GlobalMetadata global;
+	memcpy(&global, (buffer + sizeof(MAGIC_BYTES)), sizeof(GlobalMetadata));
+
+	allocator.FreeData(buffer, bytes_to_read);
+
+	return global;
+}
+
+void NvmeFileSystemProxy::WriteMetadata() {
+}
+
+uint64_t NvmeFileSystemProxy::GetLBA(MetadataType type, std::string filename) {
+}
+
+GlobalMetadata NvmeFileSystemProxy::InitializeMetadata() {
+	// Create buffer
+	// insert magic bytes
+	// insert metadata
+	// 	- We know size of db and WAL metadata and partly temp
+	// 	- We do not know the size of the file mapping field in temp
+	// 		- Should this be constant based on the size of directory?
+	//		- Dynamic?
+	// Example:
+	//  1 GB temp data -> x files -> map that supports x files total (this is the size)
+
+	idx_t bytes_to_write = sizeof(MAGIC_BYTES) + sizeof(GlobalMetadata);
+	idx_t medata_location = 0;
+
+	data_ptr_t buffer = allocator.AllocateData(bytes_to_write);
+
+	Metadata meta_db {.start = 1, .end = 5001, .location = 1};
+	Metadata meta_wal {.start = 5002, .end = 10002, .location = 5002};
+	Metadata meta_temp {.start = 10003, .end = 15003, .location = 10003};
+
+	GlobalMetadata global {.database = meta_db, .write_ahead_log = meta_wal, .temporary = meta_temp};
+
+	memcpy(buffer, MAGIC_BYTES, sizeof(MAGIC_BYTES));
+	memcpy(buffer + sizeof(MAGIC_BYTES), &global, sizeof(GlobalMetadata));
+
+	FileOpenFlags flags = FileOpenFlags::FILE_FLAGS_WRITE | FileOpenFlags::FILE_FLAGS_FILE_CREATE;
+
+	unique_ptr<FileHandle> fh = fs->OpenFile(NVME_GLOBAL_METADATA_PATH, flags);
+	fs->Write(*fh, buffer, bytes_to_write, medata_location);
+
+	allocator.FreeData(buffer, bytes_to_write);
+
+	return global;
+}
+
+} // namespace duckdb

e76785c3e17d562e65815e22b58815b08fb56f13
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-02-27 10:31:41 +0000

    Use python module

--- scripts/setup-custom-toolchain.sh
+++ scripts/setup-custom-toolchain.sh
@@ -19,7 +19,7 @@
 # note that the $DUCKDB_PLATFORM environment variable can be used to discern between the platforms
 echo "This is the sample custom toolchain script running for architecture '$DUCKDB_PLATFORM' for the nvmefs extension."
 
-pip3 install --user meson
+python -m pip install --user meson
 export PATH=$PATH:/Users/runner/Library/Python/3.11/bin
 
 

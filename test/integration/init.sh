9bd08b8a2fb8c39c594e046c7cfdeda49af7bdc8
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-03-26 18:19:36 +0000

    Use the of ducbdb python client from duckdb project

--- test/integration/init.sh
+++ test/integration/init.sh
@@ -9,6 +9,7 @@ init_environment() {
         echo "Creating environment and installing dependencies: ${current_dir}..."
         python3 -m venv "${current_dir}/.venv"
         source "${current_dir}/.venv/bin/activate"
+        python3 -m pip install "${current_dir}/../../duckdb/tools/pythonpkg/"
         python3 -m pip install -r "${current_dir}/requirements.txt"
     fi
 }

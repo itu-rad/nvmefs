9189d512d5334ec5ebfa19d44c9e9487982c9e7e
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-03-27 11:00:06 +0000

    Add initial tests

---
+++ test/integration/basic/open_and_close_database.py
@@ -0,0 +1,33 @@
+import pytest
+import duckdb
+
+def test_create_database_close_and_open(device, extension_path):
+    """
+    This tests that we can create a database with a schema and table, close it, and open it again.
+    The reason is that the database persist the data after it is closed, therefor we should be 
+    able to open it again and read the same data.   
+    """
+    duckdb.execute(f"""CREATE OR REPLACE PERSISTENT SECRET nvmefs (
+                        TYPE NVMEFS,
+                        nvme_device_path '{device.device_path}',
+                        fdp_plhdls       '{7}',
+                    );""")
+
+    con = duckdb.connect("nvmefs:///test.db", config={"allow_unsigned_extensions": "true"})
+    con.load_extension(extension_path)
+
+    con.execute("CREATE SCHEMA public;")
+    con.execute("CREATE TABLE public.numbers (a INTEGER);")
+    con.execute("INSERT INTO numbers VALUES (1), (2), (3);")
+    result = con.execute("SELECT * FROM numbers;").fetchall()
+    assert result == [(1), (2), (3)]
+
+    con.close()
+
+    con = duckdb.connect("nvmefs:///test.db", config={"allow_unsigned_extensions": "true"})
+    con.load_extension(extension_path)
+
+    result = con.execute("SELECT * FROM numbers;").fetchall()
+    assert result == [(1), (2), (3)]
+    con.close()
+    device.deallocate(1)
---
+++ test/integration/basic/temporary.py
@@ -0,0 +1,35 @@
+import pytest
+import duckdb
+
+@pytest.fixture(scope="module")
+def tpch_database_connection(device, extension_path):
+    # Setup
+    duckdb.execute(f"""CREATE OR REPLACE PERSISTENT SECRET nvmefs (
+                        TYPE NVMEFS,
+                        nvme_device_path '{device.device_path}',
+                        fdp_plhdls       '{7}',
+                    );""")
+
+    con = duckdb.connect("nvmefs:///database.db", config={"allow_unsigned_extensions": "true", "memory_limit": "500MB"})
+    con.load_extension(extension_path)
+    con.load_extension("tpch")
+
+    con.execute("CALL dbgen(sf=1);")
+
+    yield con
+
+    # Teardown the database and data
+    con.close()
+    device.deallocate(1)
+
+def test_large_query_and_spilling_to_disk(tpch_database_connection):
+    """
+    Tests that the database using nvmefs can handle large queries and spills to disk
+    """
+
+    connection = tpch_database_connection
+    expected_query7_result = connection.execute("FROM tpch_answers() WHERE query_nr = 7 AND scale_factor = 1;").fethcall()
+
+    query7_result = connection.execute("PRAGMA tpch(7);").fetchall()
+
+    assert query7_result == expected_query7_result

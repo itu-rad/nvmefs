9ee10c3666b09db2f84e32c14ddb614863acf12d
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-02 10:19:17 +0000

    Use pytest runner instead of our own

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 EXTENSION_DIR=$1
+CURRENT_DIR=$(pwd)
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "${SCRIPT_DIR}/init.sh"
 
-python3 "${SCRIPT_DIR}/test_runner.py" --extension_dir_path $EXTENSION_DIR --device "/dev/ng1n1"
+cd "${SCRIPT_DIR}"
+pytest --extension_dir_path="../../build/release/extension/nvmefs" --device="/dev/ng1n1"
+cd "${CURRENT_DIR}"

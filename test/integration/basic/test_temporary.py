2529c35916fc0b2025fb2262fd9e7ca614c38b77
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-02 11:23:44 +0000

    Use attach in the tpch query generation

--- test/integration/basic/test_temporary.py
+++ test/integration/basic/test_temporary.py
@@ -5,7 +5,7 @@ import duckdb
 def tpch_database_connection(device):
     # Setup
 
-    con = duckdb.connect("nvmefs:///database.db", config={"allow_unsigned_extensions": "true", "memory_limit": "500MB"})
+    con = duckdb.connect(config={"allow_unsigned_extensions": "true", "memory_limit": "500MB"})
     con.load_extension("nvmefs")
     con.load_extension("tpch")
 
@@ -14,6 +14,9 @@ def tpch_database_connection(device):
                         nvme_device_path '{device.device_path}',
                         fdp_plhdls       '{7}'
                     );""")
+
+    con.execute("ATTACH DATABASE 'nvmefs:///tpch.db' AS test (READ_WRITE);")
+    con.execute("USE test;")
 
     con.execute("CALL dbgen(sf=1);")
 
@@ -28,7 +31,7 @@ def test_large_query_and_spilling_to_disk(tpch_database_connection):
     """
 
     connection = tpch_database_connection
-    expected_query7_result = connection.execute("FROM tpch_answers() WHERE query_nr = 7 AND scale_factor = 1;").fethcall()
+    expected_query7_result = connection.execute("FROM tpch_answers() WHERE query_nr = 7 AND scale_factor = 1;").fetchall()
 
     query7_result = connection.execute("PRAGMA tpch(7);").fetchall()
 

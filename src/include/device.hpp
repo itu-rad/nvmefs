47a49ab7333e8209bb73be86b3d9bbbc3a83b8c2
emiloh <82398520+emiloh@users.noreply.github.com>
2025-04-02 11:50:37 +0000

    fix: abstract device to use command context for read/write

--- src/include/device.hpp
+++ src/include/device.hpp
@@ -20,8 +20,8 @@ class Device {
 	public:
 		virtual ~Device() = default;
 
-		virtual idx_t Write(void *buffer, idx_t nr_bytes, idx_t nr_lba, idx_t start_lba, idx_t offset);
-		virtual idx_t Read(void *buffer, idx_t nr_bytes, idx_t nr_lba, idx_t start_lba, idx_t offset);
+		virtual idx_t Write(void *buffer, CmdContext context);
+		virtual idx_t Read(void *buffer, CmdContext context);
 
 		virtual DeviceGeometry GetDeviceGeometry();
 

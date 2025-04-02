6ab3543704a85b9ede9be8601a2fb116d19652d7
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-02 09:22:57 +0000

    Rename test files to have a test_ prefix
    pytest only pick up files with that prefix

--- test/integration/test_runner.py
+++ test/integration/test_runner.py
@@ -37,33 +37,11 @@ class Arguments:
         
         return arguments
 
-def pytest_addoption(parser):
-
-    parser.addoption(
-        "--device_path",
-        type=str,
-        help="Path to the device to be used for the extension",
-        default="/dev/ng1n1"
-    )
-
-
-@pytest.fixture(scope="session")
-def device_path(pytestconfig):
-    return pytestconfig.getoption("device_path")
-
-@pytest.fixture(scope="module")
-def device(device_path):
-    device = NvmeDevice(device_path)
-
-    yield device
-
-    device.deallocate(1)
-
 if __name__ == "__main__":
     args = Arguments.parse_args()
     duckdb.sql(f"INSTALL nvmefs FROM '{args.extension_dir_path}';") # Ensures that when we call "LOAD nvmefs" that it can be found in the extension directory
 
-    pytest.main()
+    pytest.main(["--device_path", "hello world"])
 
     # args = Arguments.parse_args()
     # extension_filepath = os.path.join(args.extension_dir_path, "nvmefs.duckdb_extension")

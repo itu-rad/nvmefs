fd7ea9a0cbc3d2e1ae17d6f1ba2e2c41b536408a
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-03 14:10:14 +0200

    Implement fake device (#39)
    * Add initial fake device
    
    * Implement fake device
    
    * Add fake device to cmake list
    
    * Use const reference

---
+++ src/include/fake_device.hpp
@@ -0,0 +1,24 @@
+#include "nvme_device.hpp"
+
+namespace duckdb {
+constexpr idx_t DEFAULT_BLOCK_SIZE = 1ULL << 12;
+
+class FakeDevice : public Device {
+public:
+	FakeDevice(idx_t lba_count, idx_t lba_size = DEFAULT_BLOCK_SIZE);
+	~FakeDevice();
+
+	idx_t Write(void *buffer, const CmdContext &context) override;
+	idx_t Read(void *buffer, const CmdContext &context) override;
+
+	DeviceGeometry GetDeviceGeometry() override;
+
+	string GetName() const override {
+		return "FakeDevice";
+	}
+
+private:
+	const DeviceGeometry geometry;
+	vector<uint8_t> memory;
+};
+} // namespace duckdb

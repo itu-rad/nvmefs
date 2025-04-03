e6b29bd6d307af0f9fd70e7f7260fe3ac912dc01
emiloh <82398520+emiloh@users.noreply.github.com>
2025-04-03 06:46:05 +0000

    fix: function parameters match decleration

--- src/device.cpp
+++ src/device.cpp
@@ -1,11 +1,11 @@
 #include "device.hpp"
 
 namespace duckdb {
-	idx_t Device::Write(void *buffer, idx_t nr_bytes, idx_t nr_lba, idx_t start_lba, idx_t offset) {
+	idx_t Device::Write(void *buffer, CmdContext context) {
 		throw NotImplementedException("%s: Write is not implemented", GetName());
 	}
 
-	idx_t Device::Read(void *buffer, idx_t nr_bytes, idx_t nr_lba, idx_t start_lba, idx_t offset) {
+	idx_t Device::Read(void *buffer, CmdContext context) {
 		throw NotImplementedException("%s: Read is not implemented", GetName());
 	}
 

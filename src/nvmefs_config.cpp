1dd7fd05e74fc343675b9fcb220ed4370f048d45
emiloh <82398520+emiloh@users.noreply.github.com>
2025-04-10 13:47:35 +0000

    feat: add backend field to secrets (configuration)

--- src/nvmefs_config.cpp
+++ src/nvmefs_config.cpp
@@ -24,6 +24,7 @@ static unique_ptr<BaseSecret> CreateNvmefsSecretFromConfig(ClientContext &contex
 void SetNvmefsSecretParameters(CreateSecretFunction &function) {
 	function.named_parameters["nvme_device_path"] = LogicalType::VARCHAR;
 	function.named_parameters["fdp_plhdls"] = LogicalType::BIGINT;
+	function.named_parameters["backend"] = LogicalType::VARCHAR;
 }
 
 void RegisterCreateNvmefsSecretFunciton(DatabaseInstance &instance) {
@@ -54,6 +55,7 @@ NvmeConfig NvmeConfigManager::LoadConfig(DatabaseInstance &instance) {
 	KeyValueSecretReader secret_reader(instance, "nvmefs", "nvmefs://");
 
 	string device;
+	string backend;
 	int64_t plhdls = 0;
 	// TODO: ensure that we always have value here. It is possible to not have value
 	idx_t max_temp_size = 1ULL << 30; // 1 GiB
@@ -63,6 +65,7 @@ NvmeConfig NvmeConfigManager::LoadConfig(DatabaseInstance &instance) {
 	idx_t max_wal_size = 1ULL << 25; // 32 MiB
 
 	secret_reader.TryGetSecretKeyOrSetting<string>("nvme_device_path", "nvme_device_path", device);
+	secret_reader.TryGetSecretKeyOrSetting<string>("backend", "backend", backend);
 	secret_reader.TryGetSecretKeyOrSetting<int64_t>("fdp_plhdls", "fdp_plhdls", plhdls);
 
 	config.AddExtensionOption("nvme_device_path", "Path to NVMe device", {LogicalType::VARCHAR}, Value(device));
@@ -70,8 +73,37 @@ NvmeConfig NvmeConfigManager::LoadConfig(DatabaseInstance &instance) {
 	                          {LogicalType::BIGINT}, Value(plhdls));
 
 	return NvmeConfig {.device_path = device,
-	                   .plhdls = static_cast<idx_t>(plhdls),
-	                   .max_temp_size = max_temp_size,
+					   .backend = SanatizeBackend(backend),
+					   .async = IsAsynchronousBackend(backend),
+	            	   .plhdls = static_cast<idx_t>(plhdls),
+	            	   .max_temp_size = max_temp_size,
 	                   .max_wal_size = max_wal_size};
 }
+
+bool NvmeConfigManager::IsAsynchronousBackend(const string &backend) {
+	if  (StringUtil::Equals(backend.data(), "io_uring") ||
+	     StringUtil::Equals(backend.data(), "io_uring_cmd") ||
+		 StringUtil::Equals(backend.data(), "spdk_async") ||
+		 StringUtil::Equals(backend.data(), "libaio") ||
+		 StringUtil::Equals(backend.data(), "io_ring") ||
+		 StringUtil::Equals(backend.data(), "iocp") ||
+		 StringUtil::Equals(backend.data(), "iocp_th") ||
+		 StringUtil::Equals(backend.data(), "posix") ||
+		 StringUtil::Equals(backend.data(), "emu") ||
+		 StringUtil::Equals(backend.data(), "thrpool") ||
+		 StringUtil::Equals(backend.data(), "nil"))
+	{
+		return true;
+	}
+	return false;
+
+}
+
+string NvmeConfigManager::SanatizeBackend(const string &backend) {
+	if ( StringUtil::Equals(backend.data(), "spdk_async") || StringUtil::Equals(backend.data(), "spdk_sync")) {
+		return "spdk";
+	}
+	return backend;
+}
+
 } // namespace duckdb

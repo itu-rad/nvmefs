fc0cf5c565304e0833a2299b6f749c080bf4a757
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-05-06 20:19:16 +0200

    Increase the amount of blocks used for temporary data (#58)

--- src/nvmefs_config.cpp
+++ src/nvmefs_config.cpp
@@ -63,7 +63,7 @@ NvmeConfig NvmeConfigManager::LoadConfig(DatabaseInstance &instance) {
 	string backend;
 	int64_t plhdls = 0;
 	// TODO: ensure that we always have value here. It is possible to not have value
-	idx_t max_temp_size = 1ULL << 30; // 1 GiB
+	idx_t max_temp_size = 76ULL << 30; // ~76 GiB
 	if (config.options.maximum_swap_space != DConstants::INVALID_INDEX) {
 		max_temp_size = static_cast<idx_t>(config.options.maximum_swap_space);
 	}

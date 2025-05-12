5f81ccf5a1311fbe6ce7f5fafb294e73e95eae93
emiloh <82398520+emiloh@users.noreply.github.com>
2025-05-12 11:10:30 +0000

    fix: use no namespace

--- scripts/nvme/create_device.sh
+++ scripts/nvme/create_device.sh
@@ -1,3 +1,3 @@
-NUM_BLOCKS=$(nvme id-ctrl /dev/nvme1n1 | grep 'tnvmcap' | sed 's/,//g' | awk -v BS=4096 '{print $3/BS}')
+NUM_BLOCKS=$(nvme id-ctrl /dev/nvme1 | grep 'tnvmcap' | sed 's/,//g' | awk -v BS=4096 '{print $3/BS}')
 nvme create-ns /dev/nvme1 -b 4096 --nsze=$NUM_BLOCKS --ncap=$NUM_BLOCKS
 nvme attach-ns /dev/nvme1 --namespace-id=1 --controllers=0x7

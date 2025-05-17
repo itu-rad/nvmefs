50a6b6fd4a63534d3879c2b1e1915f1d98582f3d
emiloh <82398520+emiloh@users.noreply.github.com>
2025-05-17 12:59:49 +0000

    feat: update create device script and introduce create  fdp device

--- scripts/nvme/create_device.sh
+++ scripts/nvme/create_device.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 NUM_BLOCKS=$(nvme id-ctrl /dev/nvme1 | grep 'tnvmcap' | sed 's/,//g' | awk -v BS=4096 '{print $3/BS}')
+nvme set-feature /dev/nvme1 -f 0x1D -c 0 -s
 nvme create-ns /dev/nvme1 -b 4096 --nsze=$NUM_BLOCKS --ncap=$NUM_BLOCKS
 nvme attach-ns /dev/nvme1 --namespace-id=1 --controllers=0x7

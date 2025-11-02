bfabbddd40002df009b377182d6c90441315966e
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-11-02 16:29:56 +0000

    Add VCPKG and boost install
    The boost install is a temp fix until we can crack it

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -5,7 +5,9 @@ sudo bash -i ./scripts/xnvme/install.sh
 
 # Install dev dependencies
 sudo apt update
-sudo apt install -y ccache vim curl zip
+
+# TODO: Remove libboost-all-dev and use the libraries installed via vcpkg
+sudo apt install -y ccache vim curl zip libboost-all-dev
 
 # Setup tools directory
 mkdir -p ~/.tools/bin
@@ -17,6 +19,7 @@ cd vcpkg && ./bootstrap-vcpkg.sh
 VCPKG_ROOT=~/.tools/vcpkg
 
 echo "export VCPKG_ROOT='$VCPKG_ROOT'" >> ~/.bashrc
+echo "export VCPKG_TOOLCHAIN_PATH='$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake'" >> ~/.bashrc
 echo "export PATH='$PATH:$VCPKG_ROOT'" >> ~/.bashrc 
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
